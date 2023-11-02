b996a37a0498e69e7e1f8f5bc870d89c480efc58
shankar-choudhury <146004714+shankar-choudhury@users.noreply.github.com>
2023-11-02 19:22:32 -0400

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,15 +1,30 @@
 import java.util.Arrays;
 import java.util.Objects;
 
-public record Rectangle<T>(T top, T bottom, T left, T right) {
+public record Rectangle<T extends Comparable<? super T>>(T top, T bottom, T left, T right) {
 
-    public Rectangle {
+    public static <T extends Comparable<? super T>> Rectangle<T> of(T top, T bottom, T left, T right){
         verifyNonNull(top, bottom, left, right);
+        verifyBounds(bottom, top);
+        verifyBounds(left, right);
+        return new Rectangle<>(top, bottom, left, right);
     }
 
-    public final void verifyNonNull(Object... args) {
+    public static final void verifyNonNull(Object... args) {
         if (Arrays.stream(args).anyMatch(Objects::isNull))
             throw new NullPointerException();
+    }
+
+    private static final <T extends Comparable<? super T>> void verifyBounds(T lowerBound, T upperBound) {
+        verifyNonNull(lowerBound, upperBound);
+        if (lowerBound.compareTo(upperBound) >= 0)
+            throw new IllegalArgumentException("Lower bound must be less than upper bound");
+    }
+
+    static class TestHook {
+        public static <T extends Comparable<? super T>> void verifyBounds(T lowerBound, T upperBound) {
+            Rectangle.verifyBounds(lowerBound, upperBound);
+        }
     }
 
 }
